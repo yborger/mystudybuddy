f304400a9ba7fdcc7dae95877efbf1fb2d22102d
Yael Borger <100532471+yborger@users.noreply.github.com>
2026-04-10 00:41:42 +0000

    Pause

---
+++ utils.js
@@ -0,0 +1,12 @@
+/* 
+    Unit tests w/ Jest
+    This file is being added as a way of testing the usability of this extension without having to directly add it to Chrome as an extension.
+*/
+
+
+
+//blocked_choice.js tests
+function blockCheck(selections){
+    const sites = ['instagram', 'facebook', 'twitter', 'youtube', 'snapchat', 'tiktok'];
+    return sites.filter()
+}
